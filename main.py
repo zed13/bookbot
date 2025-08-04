8f46d7e895c7c8a130122d76dd6e9c927a4ccbd5
Denis Danilchuk <ddanilchuk13@gmail.com>
2025-08-04 22:30:26 +0200

    chars stat function added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from stats import get_num_words
+from stats import get_num_chars
 
 def main():
     nums_word = get_num_words("books/frankenstein.txt")
+    nums_char = get_num_chars("books/frankenstein.txt")
     print(f"{nums_word} words found in the document")
+    print(nums_char)
 
 main()
--- stats.py
+++ stats.py
@@ -3,3 +3,13 @@ def get_num_words(filepath):
         file_content = f.read()
         words_count = len(file_content.split())
         return words_count
+
+def get_num_chars(filepath):
+    stats = {}
+    with open(filepath) as f:
+        for char in f.read():
+           norm_char = char.lower()
+           if norm_char not in stats:
+             stats[norm_char] = 0
+           stats[norm_char] += 1
+    return stats
