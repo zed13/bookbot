d4e4588418ac190dae84130056c5db874b2ccb49
Denis Danilchuk <ddanilchuk13@gmail.com>
2025-08-04 22:10:18 +0200

    Get file book functionality

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_content = f.read()
+        print(file_content)
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+main()
